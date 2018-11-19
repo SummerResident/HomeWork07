7fd1ba0076a9416d7064ccd2d2cf3e3927bedd24
Paul <42929454+SummerResident@users.noreply.github.com>
2018-11-19 10:22:07 +0200

    Update Main.java

--- HomeWork07/src/lab7/Main.java
+++ HomeWork07/src/lab7/Main.java
@@ -4,12 +4,12 @@ import lab7.DepositorInformation.ContractType;
 
 public class Main {
 	public static void main(String[] args) {
-		DepositorInformation depositor1 = new DepositorInformation("Кацюк Дмитрий Генадиевич", 123455, "Красные хуторы");
+		DepositorInformation depositor1 = new DepositorInformation("РљР°С†СЋРє Р”РјРёС‚СЂРёР№ Р“РµРЅР°РґРёРµРІРёС‡", 123455, "РљСЂР°СЃРЅС‹Рµ С…СѓС‚РѕСЂС‹");
 		depositor1.chooseContract(ContractType.CUSTOMARYCONTRACT);
 		System.out.println(depositor1);
 		System.out.println("_______________________________");
 		System.out.println();
-		DepositorInformation depositor2 = new DepositorInformation("Барталев Павел Эдуардович", 123567, "дома");
+		DepositorInformation depositor2 = new DepositorInformation("Р‘Р°СЂС‚Р°Р»РµРІ РџР°РІРµР» Р­РґСѓР°СЂРґРѕРІРёС‡", 123567, "РґРѕРјР°");
 		depositor2.chooseContract(ContractType.PREMIUMCONTRACT);
 		System.out.println(depositor2);
 	}
